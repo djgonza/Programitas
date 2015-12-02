d8dfb08019f52b9eabe130738f767254d321305b
David Jimenez Gonzalez <davidjimenezgonza@gmail.com>
2015-12-02 20:02:09 +0100

    Nueva Funcionalidad
    Pide los datos por consola

--- nombreArchivo/CambiarNombre.java
+++ nombreArchivo/CambiarNombre.java
@@ -1,68 +1,119 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class CambiarNombre {
 
-	public static void leerDirectorio () {
-		String extension = "mp3";
-		//posicion para el nombre
-		int posicion = 0;
-		//leemos el directorio
-		File directorio = new File(".");
-		//leemos los archivos del directorio
-		File[] ficheros = directorio.listFiles();
+	private Scanner teclado;
+	private String cadena;
+	private String sustituto;
+	private String extension;
+	private File directorio;
+	private File[] ficheros;
+	private String salir;
+
+
+	public CambiarNombre () {
+		//inicializamos las variables
+		cadena = "";
+		sustituto = "";
+		extension = "";
+		salir = "";
+
+		//inicializamos el teclado
+		teclado = new Scanner (System.in);
+
+		//pedimos los datos y cambiamos los archivos
+		do {
+
+			//leemos el directorio
+			directorio = new File(".");
+
+			//leemos los archivos de directorio
+			ficheros = directorio.listFiles();
+
+			//pedimos los datos
+			pedirDatos();
+
+			//aplicamos los cambios
+			aplicarCambios();
+
+			//comprovamos si desea continuar
+			System.out.println("¿Desea continuar? (si/no)");
+			this.salir = teclado.nextLine();
+
+		}
+		while(salir.equals("si"));
+		
+	}
+
+	public void pedirDatos () {
+
+		System.out.println("Introduce el tipo de archivo ");
+		this.extension = teclado.nextLine();
+
+		System.out.println(extension);
+
+		System.out.println("Introduce la cadena que deseas buscar");
+		this.cadena = teclado.nextLine();
+
+		System.out.println(cadena.length());
+
+		System.out.println("Introduce la cadena que deseas reemplazar");
+		this.sustituto = teclado.nextLine();
+
+		System.out.println(sustituto.length());
+
+	}
+
+	public void aplicarCambios () {
+		
 		//recorremos los archivos
 		for (int i=0; i<ficheros.length; i++){
+			
 			//sacamos el nombre del archivo
 		  	String nombre = ficheros[i].getName();
-
-		  	System.out.println(nombre);
 
 	  		//comprobamos la extension
 		  	if(nombre.endsWith(extension)){
 
-		  		//definimos la posicion que tomara
-		  		posicion++;
-
-		  		//renombremos el archivo
-		  		String nuevoNombre = renombrar(nombre, posicion, extension);
-		  		
-		  		//variable para saber si existe el fichero
-		  		int existe = 0;
-		  		//recorremos los archivos del directorio
-		  		for (int j=0; j<ficheros.length; j++){
-		  			//si existe sumamos
-		  			if(ficheros[j].getName() == nuevoNombre)
-		  				existe++;
-		  		}
-
-				//comprobamos que no exista un archivo con el nuevo nombre
-			  	if(existe==0){
-			  		//renombramos el archivo
-			  		boolean success = ficheros[i].renameTo(new File(nuevoNombre));
-	            	if (!success)
-	                	System.out.println("Error intentando cambiar el nombre de fichero");
-			  	}else{
-			  		System.out.println("El archivo ya existe");
-			  	}
+		  		//renombramos el archivo
+			  	boolean success = ficheros[i].renameTo(new File(renombrar(nombre)));
+	            if (!success)
+	                System.out.println("Error intentando cambiar el nombre de fichero");
+			  	
 		  	}	
 		  	
 		}
 
 	}
 
-	public static String renombrar (String nombre, int posicion, String extension) {
-		
+	public String renombrar (String nombre) {
+
 		String nuevoNombre = "";
-		//nuevoNombre = nombre.replace(".", "");
-		nuevoNombre = nombre.replaceAll("mp3", ".mp3");
+
+		if(this.cadena.length() > 1){
+
+			if(this.sustituto.length() == 0) {
+				nuevoNombre = nombre.replace(this.cadena, "");
+			}else{
+				nuevoNombre = nombre.replace(this.cadena, this.sustituto);
+			}
+
+		}else{
+			if(this.sustituto.length() == 0) {
+				nuevoNombre = nombre.replaceAll(this.cadena, "");
+			}else{
+				nuevoNombre = nombre.replaceAll(this.cadena, this.sustituto);
+			}
+		}
+
 		System.out.println(nuevoNombre);
+
 		return nuevoNombre;
 	}
 
 	public static void main(String[] args) {
-		//System.out.print("Funciona!!!");
-
-		leerDirectorio();
+		CambiarNombre app = new CambiarNombre();
 	}
 
 }
